9b20891b11b6d8ed153e3ab82e864ba0f49facf5
koushik-ahmad <koushikahmad123@gmail.com>
2025-01-15 19:24:54 +0600

    implement category data by id

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // current pets
 let currentPets = [];
 
-// Fetch All Pet Categories
+// 1. Fetch All Pet Categories
 const loadCategories = () => {
   fetch(`https://openapi.programming-hero.com/api/peddy/categories`)
     .then((res) => res.json())
@@ -9,7 +9,7 @@ const loadCategories = () => {
     .catch((error) => console.log(error));
 };
 
-// Fetch All Pets
+// 2. Fetch All Pets
 const loadAllPets = () => {
   // Show Loading
   const leftContainer = document.getElementById("pets-cards-container");
@@ -26,10 +26,46 @@ const loadAllPets = () => {
   }, 2000);
 };
 
+// 3. Fetch Pets by Category form API
+const loadCategoryPets = (category) => {
+  const leftContainer = document.getElementById("pets-cards-container");
+  leftContainer.innerHTML = `<span class="loading loading-bars loading-lg col-span-3 mx-auto w-12"></span>`;
+  // Active Class to The Active Button
+  removeActiveClass();
+  const activeButton = document.getElementById(`btn-${category}`);
+  activeButton.classList.add("active");
+  // Fetch Category pet data after 2 Seconds
+  setTimeout(() => {
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+      .then((response) => response.json())
+      .then((data) => {
+        currentPets = data.data;
+        displayAllPets(currentPets);
+      })
+      .catch((error) => console.log(error));
+  }, 2000);
+};
+
 // Display All Pets by Default
 const displayAllPets = (pets) => {
   const petsCardContainer = document.getElementById("pets-cards-container");
   petsCardContainer.innerHTML = "";
+
+  if (pets.length === 0) {
+    petsCardContainer.classList.remove("grid");
+    petsCardContainer.innerHTML = `
+        <div class="text-center p-8 border-2 border-solid rounded-xl w-full h-full flex flex-col justify-center items-center bg-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="rounded-xl w-full h-full py-8" src="./images/error.webp" alt="Error Image">
+            </div>
+            <h2 class="font-extrabold text-4xl mb-4">No Information Available</h2>
+            <p class="mb-16 text-center font-semibold text-tertiaryTextColor">Currently, there is no information available in the Bird category. We are working to update our listings and provide you with the latest details. Please check back soon for new additions and updates!</p>
+        </div>
+        `;
+    return;
+  } else {
+    petsCardContainer.classList.add("grid");
+  }
 
   pets.forEach((pet) => {
     const card = document.createElement("div");
@@ -93,11 +129,19 @@ const displayCategories = (categories) => {
     buttonContainer.classList =
       "flex lg:justify-between lg:items-center mb-8 gap-2 md:gap-4";
     buttonContainer.innerHTML = `
-    <button id="btn-${item.category}"  class="categories-btn py-4 h-20 border-2 border-grey-300 rounded-2xl font-extrabold text-xl hover:border-2 hover:border-secondaryColor hover:bg-userBorderColor hover:rounded-full w-full flex items-center justify-center gap-2 lg:px-20"><img class="object-cover h-full" src="${item.category_icon}" alt="">${item.category}</button>
+    <button id="btn-${item.category}" onclick="loadCategoryPets('${item.category}')"  class="categories-btn py-4 h-20 border-2 border-grey-300 rounded-2xl font-extrabold text-xl hover:border-2 hover:border-secondaryColor hover:bg-userBorderColor hover:rounded-full w-full flex items-center justify-center gap-2 lg:px-20"><img class="object-cover h-full" src="${item.category_icon}" alt="">${item.category}</button>
     `;
     // Add Button
     categoryContainer.append(buttonContainer);
   });
+};
+
+// Remove Active Class
+const removeActiveClass = () => {
+  const buttons = document.getElementsByClassName("categories-btn");
+  for (let btn of buttons) {
+    btn.classList.remove("active");
+  }
 };
 
 loadCategories();
